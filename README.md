715501bb090dc04523794e6739eebcad3db8c708
bradley <bradley.j.griffith@gmail.com>
2013-11-01 15:03:28 -0500

    adding new feature for preselection

--- README.md
+++ README.md
@@ -5,9 +5,9 @@ Populous is a simple way to create selection inputs for forms that can have thei
 ## Basic Usage
 First off, you'll need to include the `populous.js` file in your project along with a recent copy of jQuery. ;)
 
-To use Populous with your select tags you will need to do two things. First, you will need to give a given select tag the `.populous-select` class. Additionally, you will need to add the `data-populous-options` attribute to your tag and populate it with an array of JS objects, wherein each object declares all of the keys and values you want your select options to show in a given context.
+To use Populous with your select tags you will need to do two things. First, you will need to give a given select tag the `.populous-select` class. Additionally, you will need to add the `data-populous-options` attribute to your tag and populate it with an array of JSON objects, wherein each object declares all of the keys and values you want your select options to show in a given context.
 
-For example, imagine you want your select tag to switch between showing US states and Canadian provinces. Your JS object array would look something like this:
+For example, imagine you want your select tag to switch between showing US states and Canadian provinces. Your JSON object array would look something like this:
 
 ```
 [{
@@ -98,3 +98,16 @@ $('#region-selector').useOptionsAtIndex(1); // Will show second array of options
 ```
 
 *There is a working example of a Populous select in this repo's index.html project.*
+
+## Preselect Options
+If you need to preselect an options array, or a value within an options array, you can do so by providing an additional data attribute that, again, defines a JSON object. In this case, the object can pass either or both of two key/value definitions. The key `options_index` should reference an index number from the options array you passed in from the `data-populous-options` attribute and is used to default your selection options to one of these arrays. Should you need to select an option from the visible options array, you can also use the key `value` to reference the *value* (not the text shown in the dropdown itself) of a given option.
+
+For example, if in the previous selection example, we wanted to show Canadian Provinces on load and preselect Quebec, we could do so like this:
+
+```
+<select id='region-selector' class='populous-select' data-populous-options-preselect='{"options_index": 1, "value": "QC"}' data-populous-options='
+  [{ "AL": "Alabama", "AK": "Alaska", "AZ": "Arizona", "AR": "Arkansas", "CA": "California", "CO": "Colorado", "CT": "Connecticut", "DE": "Delaware", "DC": "District of Columbia", "FL": "Florida", "GA": "Georgia", "HI": "Hawaii", "ID": "Idaho", "IL": "Illinois", "IN": "Indiana", "IA": "Iowa", "KS": "Kansas", "KY": "Kentucky", "LA": "Louisiana", "ME": "Maine", "MD": "Maryland", "MA": "Massachusetts", "MI": "Michigan", "MN": "Minnesota", "MS": "Mississippi", "MO": "Missouri", "MT": "Montana", "NE": "Nebraska", "NV": "Nevada", "NH": "New Hampshire", "NJ": "New Jersey", "NM": "New Mexico", "NY": "New York", "ND": "North Dakota", "NC": "North Carolina", "OH": "Ohio", "OK": "Oklahoma", "OR": "Oregon", "PA": "Pennsylvania", "RI": "Rhode Island", "SC": "South Carolina", "SD": "South Dakota", "TN": "Tennessee", "TX": "Texas", "UT": "Utah", "VT": "Vermont", "VA": "Virginia", "WA": "Washington", "WV": "West Virginia", "WI": "Wisconsin", "WY": "Wyoming" }, { "AB": "Alberta", "BC": "British Columbia", "MB": "Manitoba", "NB": "New Brunswick", "NL": "Newfoundland & Labrador", "NS": "Nova Scotia", "NT": "Northwest Territories", "NU": "Nunavut", "ON": "Ontario", "PE": "Prince Edward Island", "QC": "Quebec", "SK": "Saskatchewan", "YT": "Yukon" }]'>
+  <!-- options will be populated by JS -->
+</select>
+```
+
--- javascripts/populous.js
+++ javascripts/populous.js
@@ -1,7 +1,6 @@
-$(window).load(function(){
-
+$(document).ready(function() {
     ;(function(_updatable_selects, $) {
-        // Class constructor
+
         var Class = function() {
             return function(params) {
                 if ( !(this instanceof arguments.callee) ) {
@@ -11,21 +10,42 @@ $(window).load(function(){
             };
         };
 
-        var UpdatableSelect = Class();
-        UpdatableSelect.prototype = {
+        var Populous = Class();
+        Populous.prototype = {
             initialize: function(_updatable_select){
                 this.$updatable_select = $(_updatable_select);
                 this.options_array = this.$updatable_select.data('populous-options');
 
                 this.checkValidity();
-                this.useOptionsAtIndex(0); // Show initial options
+                this.initializeSelection();
             },
             checkValidity: function() {
                 if (typeof this.options_array == 'undefined' || this.options_array.length < 1) {
-                    console.error(this.$updatable_select[0] + ' is not a valid UpdatableSelect canidate. Node must define a "data-updatable-options" attribute that provides a string representation of an array of Javascript objects wherein each object is a set of options for a given select tag. Keys and values in each object represent individual select options with keys being the "value" attribute for each option and values being the text the user will see in the select dropdown.');
+                    console.error(this.$updatable_select[0] + ' is not a valid Populous canidate. Node must define a "data-populous-options" attribute that provides a string representation of an array of Javascript objects wherein each object is a set of options for a given select tag. Keys and values in each object represent individual select options with keys being the "value" attribute for each option and values being the text the user will see in the select dropdown.');
                 }
                 if (!this.$updatable_select.is('select')) {
-                    console.error(this.$updatable_select[0] + ' must be a "select" tag to be a valid UpdatableSelect candidate.');
+                    console.error(this.$updatable_select[0] + ' must be a "select" tag to be a valid Populous candidate.');
+                }
+            },
+            initializeSelection: function() {
+                this.useOptionsAtIndex(0); // Show initial options
+                var preselect_data = this.$updatable_select.data('populous-options-preselect');
+                if (typeof preselect_data != 'undefined') {
+                    var preselection_value = preselect_data.value,
+                        options_index = preselect_data.options_index;
+
+                    if (this.isValidIndex(options_index)) {
+                        this.useOptionsAtIndex(options_index);
+                    }
+                    else {
+                        this.useOptionsAtIndex(0);
+                    }
+                    if (typeof preselection_value != 'undefined') {
+                        this.selectOptionWithValue(preselection_value);
+                    }
+                }
+                else {
+                    this.useOptionsAtIndex(0);
                 }
             },
             useOptionsAtIndex: function(i) {
@@ -40,6 +60,9 @@ $(window).load(function(){
                     });
                 }
             },
+            selectOptionWithValue: function(value) {
+                this.$updatable_select.val(value);
+            },
             isValidIndex: function(i) {
                 var valid = true;
 
@@ -52,16 +75,20 @@ $(window).load(function(){
         }
 
         _updatable_selects.each(function(ix, o){
-            $(o).data('UpdatableSelect.instance', new UpdatableSelect(o));
+            $(o).data('Populous.instance', new Populous(o));
         });
 
         $.fn.useOptionsAtIndex = function(i) {
             this.each(function() {
-                $(this).data('UpdatableSelect.instance').useOptionsAtIndex(i);
+                $(this).data('Populous.instance').useOptionsAtIndex(i);
             });
         }
 
         return _updatable_selects;
 
     })($('.populous-select'), jQuery);
+
 });
+
+
+
